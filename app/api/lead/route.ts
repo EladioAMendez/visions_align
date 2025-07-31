448336a71fb14652ff973d4229f4b4181ed674b4
Eladio Mendez <eladioamendez@gmail.com>
2025-07-31 09:08:09 -0400

    fully removed mongoose

--- app/api/lead/route.ts
+++ app/api/lead/route.ts
@@ -1,13 +1,9 @@
 import { NextResponse, NextRequest } from "next/server";
-import connectMongo from "@/libs/mongoose";
-import Lead from "@/models/Lead";
 
 // This route is used to store the leads that are generated from the landing page.
 // The API call is initiated by <ButtonLead /> component
 // Duplicate emails just return 200 OK
 export async function POST(req: NextRequest) {
-  await connectMongo();
-
   const body = await req.json();
 
   if (!body.email) {
